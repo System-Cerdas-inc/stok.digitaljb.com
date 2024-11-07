3ad043bb35ad3a18e16ec447fed2595956048757
rhynarukami <rhy159753@gmail.com>
2024-11-07 21:06:58 +0800

    enh: barcode jadi serial number, aksi ubah serial number, stok ditiadakan

--- add_barang_action.php
+++ add_barang_action.php
@@ -15,7 +15,6 @@ if ($con == 'show_table') {
         $table .= '<tr>';
         $table .= '<td>' . $no++ . '</td>';
         $table .= '<td>' . $row['barcode'] . '</td>';
-        $table .= '<td>' . $row['jumlah_masuk'] . '</td>';
         $table .= '<td>
                         <button class="btn btn-warning btn-sm" type="button" onclick="edit_barcode(' . "'" . $row['barcode'] . "'" . ', ' . "'" . $row['jumlah_masuk'] . "'" . ');">Ubah</button>
                         <button class="btn btn-danger btn-sm" type="button" onclick="hapus_barcode(' . "'" . $row['id'] . "'" . ');">Hapus</button>
@@ -34,16 +33,9 @@ if ($con == 'show_table') {
     $check = mysqli_query($conn, "SELECT id, jumlah_masuk FROM barang_detil WHERE barcode = '$barcode' AND id_barang = '$kode';");
     $row = mysqli_fetch_array($check);
     if (isset($row['id'])) {
-        $stok = $row['jumlah_masuk'] + $stok;
-
-        $update = mysqli_query($conn, "UPDATE barang_detil SET jumlah_masuk = '$stok' WHERE barcode = '$barcode' AND id_barang = '$kode';");
-        if ($update) {
-            echo "berhasil";
-        } else {
-            echo "gagal";
-        }
+        echo "gagal";
     } else {
-        $sql2 = "insert into barang_detil (id_barang, barcode, jumlah_masuk, terjual, terbeli) values('$kode','$barcode','$stok', '0', '0')";
+        $sql2 = "insert into barang_detil (id_barang, barcode, terjual, terbeli) values('$kode','$barcode', '0', '0')";
         $insert = mysqli_query($conn, $sql2);
         if ($insert) {
             echo "berhasil";
@@ -57,6 +49,17 @@ if ($con == 'show_table') {
     $sql2 = "DELETE FROM barang_detil WHERE id = '" . $id . "';";
     $delete = mysqli_query($conn, $sql2);
     if ($delete) {
+        echo "berhasil";
+    } else {
+        echo "gagal";
+    }
+} elseif ($con == 'ubah_barcode') {
+    $kode = $_GET['kode'];
+    $barcode = $_GET['barcode'];
+
+    $sql2 = "UPDATE barang_detil SET barcode = '$barcode' WHERE barcode = '$kode';";
+    $update = mysqli_query($conn, $sql2);
+    if ($update) {
         echo "berhasil";
     } else {
         echo "gagal";
