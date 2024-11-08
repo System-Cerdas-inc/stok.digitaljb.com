2e6aa55c3f76f2803378a1a6b76b1ac25266df18
rhynarukami <rhy159753@gmail.com>
2024-11-08 14:32:15 +0800

    fix: cetak barcode atau sn

--- print_barcode.php
+++ print_barcode.php
@@ -1,43 +1,45 @@
-
- 
 <?php
-include  "configuration/config_barcode.php"; // include php barcode 128 class
+include "configuration/config_barcode.php";
 include "configuration/config_connect.php";
- 
-$kolom = $_POST['kolom'];  // jumlah kolom
- 
-if ($_POST['jumlah'] ==""){
-	$copy = "1";
-} else {
-	$copy = $_POST['jumlah'];
-} // jumlah copy barcode
 
-$barcode = $_POST['barcode'];
-$kode = $_POST['kode'];
+$kolom = isset($_POST['kolom']) ? intval($_POST['kolom']) : 1;
+$copy = isset($_POST['jumlah']) && $_POST['jumlah'] !== "" ? intval($_POST['jumlah']) : 1;
+$barcode = isset($_POST['barcode']) ? $_POST['barcode'] : '';
+$serial_number = isset($_POST['serial_number']) ? $_POST['serial_number'] : '';
+$generate = isset($_POST['generate']) ? $_POST['generate'] : '';
+$kode = isset($_POST['kode']) ? $_POST['kode'] : '';
 $counter = 1;
-// sql query ke database
-$sql_barcode = "SELECT * FROM barang WHERE kode='$kode' ";
-$baca_barcode = mysqli_query($conn, $sql_barcode) or die ("Gagal Query".mysqli_error());
-$data_barcode = mysqli_fetch_array($baca_barcode);
-//menampilkan hasil generate barcode
-echo"
-<table cellpadding='10'>";
-for ($ucopy=1; $ucopy<=$copy; $ucopy++) {
-if (($counter-1) % $kolom == '0') { echo "
-<tr>"; }
-echo"
-<td class='merk'>".substr($data_barcode['nama'],0,20)."";
-echo bar128(stripslashes($_POST['barcode']));
-echo "<?php echo $barcode;?>"; "</td>
-";
-if ($counter % $kolom == '0') { echo "</tr>
-"; }
-$counter++;
+
+echo "<table cellpadding='10'>";
+
+for ($ucopy = 1; $ucopy <= $copy; $ucopy++) {
+    if (($counter - 1) % $kolom === 0) {
+        echo "<tr>";
+    }
+
+    echo "<td class='merk'>";
+    if ($generate == 'SN') {
+        echo bar128(stripslashes($serial_number));
+    } else {
+        echo bar128(stripslashes($barcode));
+    }
+    echo "</td>";
+
+    if ($counter % $kolom === 0) {
+        echo "</tr>";
+    }
+
+    $counter++;
 }
-echo "</table>
-";
+
+if (($counter - 1) % $kolom !== 0) {
+    echo "</tr>";
+}
+
+echo "</table>";
 ?>
+
 <script>
 
-       //   setTimeout(function(){window.print()}, 2000);
-           </script>
+
+</script>
